33d7fb50e8755d70200b0fef663dc68438995ea3
theoa <theoa@DESKTOP-Q5INFIG>
2026-03-08 03:11:08 +0100

    Add spigot versio add playit

--- dow/Program.cs
+++ dow/Program.cs
@@ -10,7 +10,7 @@ namespace Dow
     class Program
     {
 
-        const string VERSION_LOCALE = "1.2.2x";
+        const string VERSION_LOCALE = "1.2.3x";
 
         static async Task VerifierMiseAJour()
         {
@@ -63,7 +63,9 @@ namespace Dow
             string url = "https://piston-data.mojang.com/v1/objects/64bb6d763bed0a9f1d632ec347938594144943ed/server.jar";
             string? choixVersion = null;
             string cheminJar;
+            string cheminPlayit;
             bool pathregister = false;
+            string urlPlayit;
 
             string? reponse_bat = "n";
             string cheminEula;
@@ -74,9 +76,12 @@ namespace Dow
             string urlJdk;
             string? reponse_start = "n";
             string? tout = "n";
+            string? reponse_playit = "n";
+            string? start_playit = "n";
             Dictionary<string, string>? version_choix = null;
 
             int choix = 0;
+            string fin;
 
             string? path = @"";
 
@@ -86,6 +91,7 @@ namespace Dow
                 { "1.21.8", "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar" },
                 { "1.20.4", "https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar" },
                 { "1.20.2", "https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar" },
+                { "1.20.1", "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar" },
                 { "1.19.4", "https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar" },
                 { "1.12.2", "https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar" },
                 { "1.8.9", "https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar" },
@@ -95,9 +101,11 @@ namespace Dow
             Dictionary<string, string> versions_paper = new Dictionary<string, string>()
             {
                 { "1.21.11", "https://fill-data.papermc.io/v1/objects/367f5088c7cc5c8f83cbededf4760622d4a27425be45611d3db6f11c75fac901/paper-1.21.11-126.jar" },
+                { "1.21.10", "https://fill-data.papermc.io/v1/objects/158703f75a26f842ea656b3dc6d75bf3d1ec176b97a2c36384d0b80b3871af53/paper-1.21.10-130.jar" },
                 { "1.21.8", "https://fill-data.papermc.io/v1/objects/8de7c52c3b02403503d16fac58003f1efef7dd7a0256786843927fa92ee57f1e/paper-1.21.8-60.jar" },
                 { "1.21.7", "https://fill-data.papermc.io/v1/objects/83838188699cb2837e55b890fb1a1d39ad0710285ed633fbf9fc14e9f47ce078/paper-1.21.7-32.jar" },
                 { "1.21.6", "https://fill-data.papermc.io/v1/objects/4b011f5adb5f6c72007686a223174fce82f31aeb4b34faf4652abc840b47e640/paper-1.20.6-151.jar" },
+                { "1.21.1", "https://fill-data.papermc.io/v1/objects/39bd8c00b9e18de91dcabd3cc3dcfa5328685a53b7187a2f63280c22e2d287b9/paper-1.21.1-133.jar" },
                 { "1.20.4", "https://fill-data.papermc.io/v1/objects/cabed3ae77cf55deba7c7d8722bc9cfd5e991201c211665f9265616d9fe5c77b/paper-1.20.4-499.jar" },
                 { "1.20.2", "https://fill-data.papermc.io/v1/objects/ba340a835ac40b8563aa7eda1cd6479a11a7623409c89a2c35cd9d7490ed17a7/paper-1.20.2-318.jar" },
                 { "1.20.1", "https://fill-data.papermc.io/v1/objects/234a9b32098100c6fc116664d64e36ccdb58b5b649af0f80bcccb08b0255eaea/paper-1.20.1-196.jar" },
@@ -107,21 +115,55 @@ namespace Dow
                 { "1.8.8", "https://fill-data.papermc.io/v1/objects/7ff6d2cec671ef0d95b3723b5c92890118fb882d73b7f8fa0a2cd31d97c55f86/paper-1.8.8-445.jar" },
                 { "1.7.10", "https://fill-data.papermc.io/v1/objects/33772078d92e9dbb027602da016524ef29af5b4c12eaddac1fe2465b01108185/paper-1.7.10-2025.jar" }
             };
-
+        
             Dictionary<string, string> versions_spigot = new Dictionary<string, string>()
             {
                 { "1.21.11", "https://getbukkit.org/get/AfuyQcEkLcPU9p6jitBdBkGotQvy8ghM" },
-                /*{ "1.21.8", "https://fill-data.papermc.io/v1/objects/8de7c52c3b02403503d16fac58003f1efef7dd7a0256786843927fa92ee57f1e/paper-1.21.8-60.jar" },
-                { "1.21.7", "https://fill-data.papermc.io/v1/objects/83838188699cb2837e55b890fb1a1d39ad0710285ed633fbf9fc14e9f47ce078/paper-1.21.7-32.jar" },
-                { "1.21.6", "https://fill-data.papermc.io/v1/objects/4b011f5adb5f6c72007686a223174fce82f31aeb4b34faf4652abc840b47e640/paper-1.20.6-151.jar" },
-                { "1.20.4", "https://fill-data.papermc.io/v1/objects/cabed3ae77cf55deba7c7d8722bc9cfd5e991201c211665f9265616d9fe5c77b/paper-1.20.4-499.jar" },
-                { "1.20.2", "https://fill-data.papermc.io/v1/objects/ba340a835ac40b8563aa7eda1cd6479a11a7623409c89a2c35cd9d7490ed17a7/paper-1.20.2-318.jar" },
-                { "1.20.1", "https://fill-data.papermc.io/v1/objects/234a9b32098100c6fc116664d64e36ccdb58b5b649af0f80bcccb08b0255eaea/paper-1.20.1-196.jar" },
-                { "1.19.4", "https://fill-data.papermc.io/v1/objects/e587d78cba3e99ef8c4bc24cf20cc3bdbbe89e33b0b572070446af4eb6be5ccf/paper-1.19.4-550.jar" },
-                { "1.19.3", "https://fill-data.papermc.io/v1/objects/3007f2c638d5f04ed32b6adaa33053fe3634ccfa74345c83d3ea4982d38db5dc/paper-1.19.3-448.jar" },*/
+                { "1.21.8", "https://getbukkit.org/get/8smOqbVnSl8RvSvlvkSNsUxEv0Y3g7Vo" },
+                { "1.21.5", "https://getbukkit.org/get/cNW08KHVlCEwof2IkXbxXIKeDPbfgMBU" },
+                { "1.21.4", "https://getbukkit.org/get/vUvveVKWgnYEO4zC7ey2pqkGtaAxvS6v" },
+                { "1.21.3", "https://getbukkit.org/get/RnpgqIvNyXe7nFTZZJyIkXl4shuhFUXm" },
+                { "1.21.1", "https://getbukkit.org/get/yqv2djplb2mijkqTIUUmC7F6pbJMpFdX" },
+                { "1.20.6", "https://getbukkit.org/get/QRmEAkJCu1HcZkgxVSmVgHZYwZ03Ua1R" },
+                { "1.20.4", "https://getbukkit.org/get/vdYtG2jRHgslyLJLaIUK2xVAL95LshGI" },
+                { "1.20.2", "https://getbukkit.org/get/ZcRnnYaSJs89j5TUCLdyXVsmem6ovqnn" },
+                { "1.20.1", "https://getbukkit.org/get/U9uE8nD9E8ubIns3NuTK6rnKrUVOVc45" },
+                { "1.19.4", "https://getbukkit.org/get/XYtMHSKmmb8UNCj2RNwXmGjRvHISRwoj" },
+                { "1.19.3", "https://getbukkit.org/get/h5zwRyDERLQVgY9ROBNlcyLLRzEsWSjH" },
+                { "1.19.2", "https://getbukkit.org/get/OaTGj2mTbLKl9o6qGXn2wnCZBdwW3Yk6" },
+                { "1.19.1", "https://getbukkit.org/get/SCPNVGrhmI4uSfXNOPi0N6AAvCtYTHgu" },
+                { "1.19", "https://getbukkit.org/get/YH4OAZAZumfJ6EisLG4xfU9op3WvEEkc" },
+                { "1.18.2", "https://getbukkit.org/get/M3CuBYuR72VaQB3W6T2TTbPiklfqSn1u" },
+                { "1.18.1", "https://getbukkit.org/get/smdDWNBN1RV5KfOacaJvnDOlIf5BdAJN" },
+                { "1.18", "https://getbukkit.org/get/DExv10cl86CUN62iB1Df4NVdG7GD1pNV" },
+                { "1.16.5", "https://getbukkit.org/get/GSXo3m2tDdbXkJA5QAw0vLihBnEEP55G" },
+                { "1.16.4", "https://getbukkit.org/get/iuObpmqGVCpBoU3BC823H5b2jKx3muCI" },
+                { "1.16.3", "https://getbukkit.org/get/ZJZrBToD0A3qOwOG6FTaZ8j3ybrKe3oJ" },
+                { "1.16.2", "https://getbukkit.org/get/WRBd7m29OQ4AmKDEOAU1C8ijgJrrAUio" },
+                { "1.16.1", "https://getbukkit.org/get/1mFuDJYv8k9kpLvY4eyZ6bstqyXjHBdx" },
+                { "1.15.2", "https://getbukkit.org/get/mpe1uTseEONyg3iFtJQaitrEoIFic75N" },
+                { "1.15.1", "https://getbukkit.org/get/OT1jS2PYF5YnHuHbdjKBiPKBXGHzPThb" },
+                { "1.15", "https://getbukkit.org/get/YeJiojZ6jVPQefdfxCT2ZzXXkHh3mmIn" },
+                { "1.14.4", "https://getbukkit.org/get/rZlZWBTsIJauWb20uqHQTjIuv0ayKTMP" },
+                { "1.14.3", "https://getbukkit.org/get/wKZI0DrixwfUBWAsiiyjqGLFpuKQ0uWo" },
+                { "1.14.2", "https://getbukkit.org/get/npSdlqa7cDVGaQo62CjoqY4ZzqIJkKqz" },
+                { "1.14.1", "https://getbukkit.org/get/h3hm8JHO3SlF0DP7RQMA4DwCm9mlK9l2" },
+                { "1.14", "https://getbukkit.org/get/BT1o9lrMqDMj3hnXlRuHotimk7zX2F7g" },
+                { "1.13.2", "https://getbukkit.org/get/jh3h8QSvol0gXPladGNin80nGid4DmWv" },
+                { "1.13.1", "https://getbukkit.org/get/BxDmQtGsHUacEIUrXaJ5fy0m3Xp19A6i" },
+                { "1.13", "https://getbukkit.org/get/pceKF1UGA4wPlu67X2lPMg4dfnhCjjb7" },
                 { "1.12.2", "https://getbukkit.org/get/Uov36RPe0zZBdi42t7OwtMFq3qaCXNyT" },
-               /* { "1.8.8", "https://fill-data.papermc.io/v1/objects/7ff6d2cec671ef0d95b3723b5c92890118fb882d73b7f8fa0a2cd31d97c55f86/paper-1.8.8-445.jar" },
-                { "1.7.10", "https://fill-data.papermc.io/v1/objects/33772078d92e9dbb027602da016524ef29af5b4c12eaddac1fe2465b01108185/paper-1.7.10-2025.jar" }*/
+                { "1.12.1", "https://getbukkit.org/get/OuENqxqgWDRSeS1Ec00KvnAjiLqRj57C" },
+                { "1.12", "https://getbukkit.org/get/DWfRX0AnTBtgWJY7CbsLXcFJKj8HUCu7" },
+                { "1.11.2", "https://getbukkit.org/get/a71XhriEGe5uvifgsghIIIZQKz549qtZ" },
+                { "1.11", "https://getbukkit.org/get/g7znNa4ffhmOuMwZajenBNc2y5fFNYdi" },
+                { "1.10.2", "https://getbukkit.org/get/emoWettOnCWTmPquJ86DpVh06tFEVCio" },
+                { "1.9.4", "https://getbukkit.org/get/UZT6lu9xerOcWKTjKYdv3sIDVU2QGhf5" },
+                { "1.9.2", "https://getbukkit.org/get/1VJgmwMfix4Qo2VJ7McbDo1ZekCPWMk6" },
+                { "1.9", "https://getbukkit.org/get/aI5o1m9YfyNOu8yGzX9LvtYmXeWSfNZU" },
+                { "1.8.8", "https://getbukkit.org/get/xo7CwyaiWNY7Ghtj3fR8bRCehoqLb5Pi" },
+                { "1.8.3", "https://getbukkit.org/get/9rrkoYWkmC1MT0frDxNuAqYUogr9hfxR" },
+                { "1.8", "https://getbukkit.org/get/J2wgWzOlirjx2drpuF4ncCxvVwqDR5p0" }
             };
 
             Console.WriteLine(" ");
@@ -140,9 +182,13 @@ namespace Dow
                     //Console.WriteLine("Ou mettez directement le nom dossier et il se créera au meme endroit que ce programme");
                     Console.Write("> ");
                     path = Console.ReadLine();
-                    if (path == "")
+                    if (path == "" )
                     {
                         Console.WriteLine("Le chemin est null");
+                    }
+                    else if (!Directory.Exists(path))
+                    {
+                        Console.Write("Le dossier n'exite pas");
                     }
                     if (path == "exit")
                     {
@@ -164,7 +210,9 @@ namespace Dow
                 Console.WriteLine("3.Créer seulement .bat");
                 Console.WriteLine("4.Télécharger et installer le JDK");
                 Console.WriteLine("5.Démarrer le serveur");
-                Console.WriteLine("6.Fermer le programme");
+                Console.WriteLine("6.Installer Playit");
+                Console.WriteLine("7.Démarrer Playit");
+                Console.WriteLine("8.Fermer le programme");
 
                 Console.Write("> ");
                 choix = Convert.ToInt32(Console.ReadLine()); //!= null ? Convert.ToInt32(Console.ReadLine()) : 0;
@@ -172,8 +220,6 @@ namespace Dow
                 if (havedir == "n")
                 {
                     Console.WriteLine(" ");
-                    Console.WriteLine("Entrez le chemin du dossier du serveur : ex C:\\Users\\Prenom\\Desktop\\NOMDUDOSSIER");
-                    Console.WriteLine("Ou mettez directement le nom dossier et il se créera au meme endroit que ce programme");
                     choix = 1;
                     pathregister = true;
                 }
@@ -186,6 +232,9 @@ namespace Dow
                         reponse_jar = "y";
                         reponse_jdk = "y";
                         reponse_start = "y";
+                        reponse_playit = "y";
+                        start_playit = "y";
+                        havedir = "y";
                         break;
                     case 2:
                         reponse_jar = "y";
@@ -197,10 +246,17 @@ namespace Dow
                         reponse_jdk = "y";
                         break;
                     case 5:
-                        reponse_start = "y";
+                        Process.Start("powershell", "cd '" + path + "' ; start start.bat");
                         break;
                     case 6:
+                        reponse_playit = "y";
+                        break;
+                    case 7:
+                        Process.Start("powershell", "cd '" + path + "' ; start Playit.exe");
+                        break;
+                    case 8:
                         Console.WriteLine("Fermeture du programme...");
+                        fin = Console.ReadLine();
                         return;
                 }
 
@@ -214,6 +270,10 @@ namespace Dow
                     createdir = Console.ReadLine();
                     if (createdir == "y")
                     {
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Entrez le chemin du dossier du serveur : ex C:\\Users\\Prenom\\Desktop\\NOMDUDOSSIER");
+                        Console.WriteLine("Ou mettez directement le nom dossier et il se créera au meme endroit que ce programme");
+
                         while (Directory.Exists(path) || path == "")
                         {
                             Console.Write("> ");
@@ -227,7 +287,9 @@ namespace Dow
                             if (path == "exit")
                             {
                                 Console.WriteLine("Fermeture du programme");
+                                fin = Console.ReadLine();
                                 return;
+                                
                             }
                             if (Directory.Exists(path))
                             {
@@ -247,7 +309,9 @@ namespace Dow
                     {
                         Console.WriteLine("Le programme n'a donc aucun dossier ou télécharger les modules");
                         Console.WriteLine("Fermeture du programme...");
+                        fin = Console.ReadLine();
                         return;
+                        
                     }
                 }
                     // Téléchargement du fichier .jar du serveur minecraft
@@ -438,6 +502,7 @@ namespace Dow
 
 
                         // Vérifier que le JDK est installé
+                    reponse_jdk = "n";
                 }
                     // Lance un cmd pour démarrer le start.bat
                     //Process.Start(Path.Combine(path, "start.bat"));
@@ -453,7 +518,56 @@ namespace Dow
                             Process.Start("powershell", "cd '" + path + "' ; start start.bat");
                             reponse_start = "n";
                 }
-            
+
+
+                if (reponse_playit == "y")
+                {
+                    Console.WriteLine("Voulez-vous télécharger Playit ? (y/n)");
+                    Console.Write("> ");
+                    reponse_playit = Console.ReadLine();
+
+                    urlPlayit = "https://github.com/playit-cloud/playit-agent/releases/download/v0.17.1/playit-windows-x86_64-signed.exe";
+
+                    Console.WriteLine(" ");
+
+                    Console.WriteLine($"Téléchargement de Playit dans {path}");
+
+                    // Le met dans le dossier créé précédement et le renomme en jdk-21.exe
+                    cheminPlayit = Path.Combine(path, "Playit.exe");
+
+                    // Voir doc Microsoft > HttpClient
+                    using (HttpClient client = new HttpClient())
+                    using (HttpResponseMessage response = await client.GetAsync(urlPlayit))
+                    {
+                        response.EnsureSuccessStatusCode();
+
+                        using (FileStream fs = new FileStream(cheminPlayit, FileMode.Create))
+                        {
+                            await response.Content.CopyToAsync(fs);
+                        }
+                    }
+
+                    Console.WriteLine("Installation terminée !");
+
+                     Console.WriteLine("Penser à le lancer en même temps que votre serveur");
+                     Console.WriteLine("sinon vos amis ne pourront pas vous rejoindre");
+                     Console.WriteLine("Playit retrouvable ici > https://playit.gg/");
+
+                    reponse_playit = "n";
+                }
+
+                if (start_playit == "y")
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Voulez-vous démarrer Playit Maintenant ? (y/n)");
+                    Console.Write("> ");
+                    reponse_start = Console.ReadLine();
+                }
+                if (start_playit == "y")
+                {
+                    Process.Start("powershell", "cd '" + path + "' ; start Playit.exe");
+                    reponse_start = "n";
+                }
             }
         }
     }
